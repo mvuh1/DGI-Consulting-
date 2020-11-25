2d65336a5953607e40dbe4e17ed53f40760008b9
nazir mvuh <nazirmvuh69gmail.com>
2020-11-25 17:02:03 +0100

    ajout image d'accueil

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 	<section class="parallax section section-parallax parallax-disabled section-parallax-home"
-		data-plugin-parallax data-image-src="/images/home/panoramique.jpg">
+		data-plugin-parallax data-image-src="/images/home_image.png">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 					<p class="baseline-1" style="font-size: 50px">DGI CONSULTING - AFRICA</p>
-					<p class="baseline-3">
+					<p class="baseline-3" style="color: ivory">
 						DUCHENE GROUP INTERNATIONAL LTD
 						DUPA LTD Velibaba MAH. YAKACIK CAD. N°132/1 A1 Block D.13 PENDIK ISTANBUL
 					</p>
@@ -69,6 +69,62 @@
 									data-appear-animation="fadeInUp" data-appear-animation-delay="900">
 									<div class="box-content">
 										<img src="{{ asset('images/img4.png') }}" class="img-responsive"
+                                        alt="Gildas Perrot, Président Associé - Consultant et formateur">
+										<h4><a href="votre-projet/partager-les-savoirs/6/22.html">Partager les
+												savoirs</a></h4>
+										<p><a href="votre-projet/partager-les-savoirs/6/22.html">Transfert,
+												Tutorat, Formateur, Plateforme d'échange ...</a></p>
+
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="featured-box featured-box-primary appear-animation mt-md"
+									data-appear-animation="fadeInUp" data-appear-animation-delay="0">
+									<div class="box-content">
+										<img src="{{ asset('images/img5.png') }}" class="img-responsive"
+                                        alt="Gildas Perrot, Président Associé - Consultant et formateur">
+										<h4><a href="votre-projet/mobiliser-les-energies/6/19.html">Mobiliser
+												les énergies</a></h4>
+										<p><a href="votre-projet/mobiliser-les-energies/6/19.html">Gouvernance,
+												Change, Leadership, Organisation, Culture ...</a></p>
+
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="featured-box featured-box-primary appear-animation mt-md"
+									data-appear-animation="fadeInUp" data-appear-animation-delay="300">
+									<div class="box-content">
+										<img src="{{ asset('images/img6.png') }}" class="img-responsive"
+                                        alt="Gildas Perrot, Président Associé - Consultant et formateur">
+										<h4><a href="votre-projet/developper-les-talents/6/20.html">Développer
+												les talents</a></h4>
+										<p><a href="votre-projet/developper-les-talents/6/20.html">Formation,
+												Coaching, Assessment, Teambuilding ...</a></p>
+
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="featured-box featured-box-primary appear-animation mt-md"
+									data-appear-animation="fadeInUp" data-appear-animation-delay="600">
+									<div class="box-content">
+										<img src="{{ asset('images/img7.png') }}" class="img-responsive"
+                                        alt="Gildas Perrot, Président Associé - Consultant et formateur">
+										<h4><a href="votre-projet/penser-le-travail/6/21.html">Repenser le
+												travail</a></h4>
+										<p><a href="votre-projet/penser-le-travail/6/21.html">Qualité de vie au
+												travail, GPEC, RSE, Ingénierie sociale ...</a></p>
+
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="featured-box featured-box-primary appear-animation mt-md"
+									data-appear-animation="fadeInUp" data-appear-animation-delay="900">
+									<div class="box-content">
+										<img src="{{ asset('images/img8.png') }}" class="img-responsive"
                                         alt="Gildas Perrot, Président Associé - Consultant et formateur">
 										<h4><a href="votre-projet/partager-les-savoirs/6/22.html">Partager les
 												savoirs</a></h4>
